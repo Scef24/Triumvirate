d918770aa6bebceeaae24fdb79ca1bb046495a09
Sce24 <christianochea@gmail.com>
2024-04-14 14:49:16 +0800

    Login,Registration Backend Done

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const mysql = require('mysql2')
+const bcrypt = require('bcrypt')
+const express = require('express')
+const jwt = require('jsonwebtoken');
+const port = 3000
+const app = express()
 
+app.use(express.json())
 
+const saltrounds = 10;
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
@@ -16,4 +27,58 @@ connection.connect(err=>{
 {
     console.log('Success')
 }
+})
+
+app.post('/register',(req,res)=>{
+   const {email,password} = req.body
+
+   bcrypt.hash(password,10,(err,hash)=>{
+    if(err) {
+        res.json({message:'Error hashing password', err})
+    }
+    else {
+        connection.query('insert into users (email,password) values (?,?)',[email,hash],(err,results)=>{
+            if(err) {
+                res.json({message:'error',err})
+            }else {
+                res.json({message:'Success'})
+            }
+        })
+    }
+   })
+})
+app.get('/users', async (req,res)=>{
+    connection.query('select * from users',(err,results)=>{
+        if(err){
+            res.json({message:'Error retrieving users',err})
+        }else{
+            res.json(results)
+        }
+    })
+})
+
+app.post('/login',(req,res)=>{
+    const {email,password} = req.body
+
+    connection.query('select * from users where email = ?',[email],async (err,results)=>{
+        if(err) {
+            console.log('Error Retrieving data',err)
+            return res.status(500).json({error:'Internal Server Error'})
+        }
+
+        if(results.length===0 ) {
+            return res.status(401).json({error:'Invalid Credentials'})
+        }
+
+        const user = results[0]
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if(!passwordMatch) {
+            return res.status(401).json({error:'Invaliid Credentials'})
+        }
+
+        const token = jwt.sign({userId:user.id, email:user.email},'secret_key')
+
+        res.json({token})
+    })
 })
